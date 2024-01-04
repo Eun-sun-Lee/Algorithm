97a415a41d031e2588fa0521d277318e4d36aa5b
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2024-01-04 21:25:23 +0900

    [Silver IV] Title: 별 찍기 - 19, Time: 96 ms, Memory: 34184 KB -BaekjoonHub

---
+++ 백준/Silver/10994. 별 찍기 － 19/별 찍기 － 19.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+
+cnt=0
+n = 4*(int(sys.stdin.readline()))-3
+queue = deque()
+print(n*'*')
+for i in range(n-2,-1,-2):
+    if cnt%2==0:
+        queue.append("*")
+    else:
+        queue.append(" ")
+    for j in queue:
+        print(j,end='')
+    if cnt%2==0:
+        print(i*" ",end='')
+    else:
+        print(i*"*",end='')
+    for j in range(len(queue)-1,-1,-1):
+        print(queue[j],end='')
+    print()
+    cnt+=1
+if n!=1:
+    cnt=0
+    for i in range(3,n,2):
+        queue.pop()
+        for j in queue:
+            print(j,end='')
+        if cnt%2==0:
+            print(i*" ",end='')
+        else:
+            print(i*"*",end='')
+        for j in range(len(queue)-1,-1,-1):
+            print(queue[j],end='')
+        print()
+        cnt+=1
+    print(n*'*')
