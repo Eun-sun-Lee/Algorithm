22626958cf11aaa4621a29c081e3e9908ceebfb4
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2024-01-04 21:19:52 +0900

    [Silver V] Title: 피보나치 수 4, Time: 72 ms, Memory: 35044 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
@@ -0,0 +1,11 @@
+# 피보나치 수4 (실버5) 
+import sys
+n = int(sys.stdin.readline())
+dp = (n + 1) * [0]
+dp[0] = 0
+if n != 0:
+    dp[1] = 1
+
+for i in range(2, n + 1):
+    dp[i] = (dp[i - 1] + dp[i - 2])
+print(dp[n])
