43948fdfc05a04d4d580853edece166c8a4110b9
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2024-01-10 01:00:05 +0900

    [Silver III] Title: N과 M (1), Time: 184 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,11 +1,20 @@
-import itertools
+# N과 M (1) (실버 3)
+import sys
+n, m = map(int, sys.stdin.readline().split())
+result = [0] * m
+arr = [i + 1 for i in range(n)]
+checked = [False for _ in range(n)]
 
-n, m = map(int, input().split())
-nums = [i for i in range(1, n+1)]
 
-array = itertools.permutations(nums, m)
+def dfs(level):
+    if level == m:
+        print(*result)
+        return
 
-for i in array:
-    for j in i:
-        print(j, end = ' ')
-    print()
+    for i in range(n):
+        if checked[i] == True: continue
+        result[level] = arr[i]
+        checked[i] = True
+        dfs(level + 1)
+        checked[i] = False
+dfs(0)
