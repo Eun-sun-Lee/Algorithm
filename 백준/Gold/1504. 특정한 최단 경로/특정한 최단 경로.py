673bdbdd0023929ab2540f21fb3a6df1ba26ffe6
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2024-02-24 10:43:35 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 512 ms, Memory: 64400 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -1,9 +1,7 @@
 import sys
 import heapq
-import copy
 node, edge = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(node + 1)]
-visited = [[False] for _ in range(node + 1)]
 
 for i in range(edge):
     u, v, w = map(int, sys.stdin.readline().split())
@@ -26,28 +24,13 @@ def dijkstra(graph, start, end):
     return distance[end]
 
 answer = sys.maxsize
-l = [1, first, first, second, second, node]
-for i in range(4):
+l = [1, first, second, node]
+for i in range(2):
     cnt = 0
     for j in range(3):
-        j *= 2
         cnt += dijkstra(graph, l[j], l[j + 1])
-        if i == 2 and j ==0:
-            cnt *= 2
-        elif i == 3 and j == 0:
-            cnt *= 2
     answer = min(answer, cnt)
-    if i == 0:
-        l = [1, second, second, first, first, node]
-    elif i == 1:
-        l = [1, second, 1, first, first, node]
-    elif i == 2:   
-        l = [1, first, 1, second, second, node]     
+    l = [1, second, first, node]
+   
 
 print(-1 if answer == sys.maxsize else answer)
-# dijkstra 2번
-# 1->2, 2->3, 3->4
-# 1->3, 3->2, 2->4
-
-# 1->2 1->3 2->4
-# 1->2 1->3 3->4
