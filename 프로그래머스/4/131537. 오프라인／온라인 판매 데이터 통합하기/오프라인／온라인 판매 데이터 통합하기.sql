f42ca4a998b805bd88c5a0b2453b4f3729e4e019
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2024-05-25 21:39:46 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,18 +1,16 @@
+# SELECT date_format(online_sale.SALES_DATE, '%Y-%m-%d') SALES_DATE, online_sale.PRODUCT_ID, online_sale.USER_ID, online_sale.SALES_AMOUNT
+# from online_sale left join offline_sale on online_sale.PRODUCT_ID=offline_sale.PRODUCT_ID and online_sale.SALES_DATE=offline_sale.SALES_DATE
+# where YEAR(online_sale.SALES_DATE) = 2022 AND MONTH(online_sale.SALES_DATE) = 3
+# order by SALES_DATE, PRODUCT_ID, USER_ID 
 
-SELECT DATE_FORMAT(SALES_DATE,"%Y-%m-%d") SALES_DATE,
-    PRODUCT_ID,
-    USER_ID,
-    SALES_AMOUNT
-FROM ONLINE_SALE
-WHERE sales_date >= '2022-03-01' and sales_date < '2022-04-01'
+select date_format(sales_date, '%Y-%m-%d') sales_date, product_id, user_id, sales_amount
+from online_sale
+where YEAR(sales_date) = 2022 AND MONTH(sales_date) = 3
 
-UNION ALL
+union all
 
-SELECT DATE_FORMAT(SALES_DATE,"%Y-%m-%d") SALES_DATE,
-    PRODUCT_ID,
-    NULL AS USER_ID,
-    SALES_AMOUNT
-FROM OFFLINE_SALE 
-WHERE sales_date >= '2022-03-01' and sales_date < '2022-04-01'
-    
-ORDER BY SALES_DATE , PRODUCT_ID , USER_ID
+select date_format(sales_date, '%Y-%m-%d') sales_date, product_id, null as user_id, sales_amount
+from offline_sale
+where YEAR(sales_date) = 2022 AND MONTH(sales_date) = 3
+
+order by sales_date, product_id, user_id
