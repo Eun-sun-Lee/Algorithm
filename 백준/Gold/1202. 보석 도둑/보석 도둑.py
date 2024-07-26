54ce2c1cc1153c11dae43e663f054793d76b836b
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2024-07-26 20:52:04 +0900

    [Gold II] Title: 보석 도둑, Time: 1540 ms, Memory: 87136 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -1,22 +1,30 @@
-import sys
-import heapq
+import sys 
+import heapq 
 
 N, K = map(int, sys.stdin.readline().split())
-jew = []
-for _ in range(N):
-    heapq.heappush(jew, list(map(int, sys.stdin.readline().split())))
-bags = []
-for _ in range(K):
-    bags.append(int(sys.stdin.readline()))
-bags.sort()
+jewel = []
+bag = []
 
-answer = 0
-tmp_jew = []
-for bag in bags:
-    while jew and bag >= jew[0][0]:
-        heapq.heappush(tmp_jew, -heapq.heappop(jew)[1])
-    if tmp_jew:
-        answer -= heapq.heappop(tmp_jew)
-    elif not jew:
-        break
-print(answer)
+for _ in range(N):
+    weight, price = map(int, sys.stdin.readline().split())
+    heapq.heappush(jewel, (weight, price))
+
+for _ in range(K):
+    weight = int(sys.stdin.readline())
+    bag.append(weight)
+bag.sort()
+
+
+def sol(jewel, bags):
+    result = 0
+    candidates = []
+    for bag in bags:
+        while jewel and jewel[0][0] <= bag:
+            w, p = heapq.heappop(jewel)
+            heapq.heappush(candidates, -p) 
+        if candidates:
+            result += -heapq.heappop(candidates)
+
+    print(result)
+    
+sol(jewel, bag)
