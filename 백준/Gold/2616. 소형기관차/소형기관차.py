12e537e1ba9c87d7556ccf049150431b7509f8f4
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2024-07-28 21:57:57 +0900

    [Gold III] Title: 소형기관차, Time: 164 ms, Memory: 35240 KB -BaekjoonHub

--- 백준/Gold/2616. 소형기관차/소형기관차.py
+++ 백준/Gold/2616. 소형기관차/소형기관차.py
@@ -1,19 +1,17 @@
-N = int(input())
-guest_list = list(map(int, input().split()))
+import sys
+n = int(sys.stdin.readline()) #기관차가 끌고 가던 객차의 수
+people = [0] + list(map(int, sys.stdin.readline().split()))
+m = int(sys.stdin.readline()) #소형기관차가 최대로 끌 수 있는 객차의 수
+dp = [[0 for j in range(n+1)] for i in range(4)] #4 = 누적합 1칸 + 소형기관차 3대
 
-limit = int(input())
-val = sum(guest_list[:limit])
-guest_sum = [val]
-
-for i in range(N-limit) :
-  val += guest_list[i+limit] - guest_list[i]
-  guest_sum.append(val)
-
-dp = [[0]*(4) for _ in range(N+1)]
-for i in range(1, N+1) :
-  for j in range(3) :
-    dp[i][j] = dp[i-1][j]
-    if i >= limit :
-      dp[i][j] = max(dp[i][j], dp[i-limit][j+1] + guest_sum[i-limit])
-
-print(dp[-1][0])
+for i in range(4):
+    for j in range(1, n+1):
+        if i == 0: #누적합
+            dp[i][j] = dp[i][j-1] + people[j]
+        elif j < i*m:
+            continue
+        elif i == 1 and j >= i*m:
+            dp[i][j] = max(dp[i][j-1], dp[0][j]-dp[0][j-m])
+        else:
+            dp[i][j] = max(dp[i][j-1], (dp[i-1][j-m] + dp[0][j] - dp[0][j-m]))
+print(dp[-1][-1])
