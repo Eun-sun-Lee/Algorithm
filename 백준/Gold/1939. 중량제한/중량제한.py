f03544515784b2bf853f7bb1d33c1e178837049b
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2024-08-11 18:29:34 +0900

    [Gold III] Title: 중량제한, Time: 236 ms, Memory: 49752 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.py
+++ 백준/Gold/1939. 중량제한/중량제한.py
@@ -1,45 +1,54 @@
-from collections import deque
-import sys
-input = sys.stdin.readline
+import sys 
+import heapq 
 
-def bfs(weight): # weight == now
-	queue = deque()
-	queue.append(one)
-	visited = [False] * (n+1)
-	visited[one] = True
-	
-	while queue:
-		x= queue.popleft() # w == limit
-		
-		for i,w in bridges[x]:
-			if not visited[i] and w >= weight:
-				visited[i] = True
-				queue.append(i)
-	
-	if visited[two]: return True
-	else: return False
-	
-n, m = map(int,input().split())
-bridges = [[] for _ in range(n+1)]
+N, M = map(int, sys.stdin.readline().split())
+graph = []
+parent = [0] * (N + 1)
 
-for i in range(m):
-	a,b,c = map(int,input().split())
-	bridges[a].append([b,c])
-	bridges[b].append([a,c])
-	
-one, two = map(int,input().split())
+for _ in range(M):
+    v1, v2, w = map(int, sys.stdin.readline().split())
+    graph.append((-w, v1, v2))
+start, end = map(int, sys.stdin.readline().split())
+graph.sort()
 
-start = 1
-end = 1000000000
+def init(N):
+    for i in range(1, N + 1):
+        parent[i] = i 
 
-result = 0
-while start <= end:
-	mid = (start + end) //2
-	
-	if bfs(mid):
-		result = mid
-		start = mid + 1
-	else:
-		end = mid - 1
-		
-print(result)
+def find(i):
+    while parent[i] != i:
+        parent[i] = parent[parent[i]]
+        i = parent[i]
+    return i
+
+def in_same_set(p, q):
+    return find(p) == find(q)
+
+def union(p, q):
+    i = find(p)
+    j = find(q)
+
+    if i != j:
+        # parent[max(i, j)] = min(i, j) # 오른쪽이 부모
+        parent[i] = j
+
+def kruskal(N, M, graph, start, end):
+    global parnet 
+    init(N)
+    
+
+    for w, v1, v2 in graph:
+        p = find(v1)
+        q = find(v2)
+
+        if in_same_set(p, q) == False:
+            union(p, q)
+            
+            if in_same_set(start, end):
+                print(-w)
+                return
+
+kruskal(N, M, graph, start, end)
+    
+
+
