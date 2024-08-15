c972908e827bab8a7f56ce364ceaafd93d6a7e35
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2024-08-15 16:43:04 +0900

    [Gold V] Title: 행복 유치원, Time: 208 ms, Memory: 66600 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.py
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.py
@@ -1,18 +1,14 @@
-import sys
+import sys 
+n, s = map(int, sys.stdin.readline().split())
+arr = list(map(int, sys.stdin.readline().split()))
 
-n, k = map(int, sys.stdin.readline().split())
-heights = list(map(int, sys.stdin.readline().split()))
+def sol(n, s, arr):
+    minusArr = []
 
-# 차이 배열을 구합니다.
-diff = []
-for i in range(1, n):
-    diff.append(heights[i] - heights[i - 1])
+    for i in range(1, n):
+        minusArr.append((arr[i] - arr[i - 1]))
+    # print(minusArr)
+    minusArr.sort()
+    print(sum(minusArr[:n - s]))
 
-# 차이 배열을 오름차순으로 정렬합니다.
-diff.sort()
-
-# 상위 (n-k)개의 차이만을 사용하면 됩니다.
-# 그 차이들의 합이 최소화된 비용이 됩니다.
-result = sum(diff[:n-k])
-
-print(result)
+sol(n, s, arr)
