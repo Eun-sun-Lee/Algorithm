7c4eed6b99e5d8781fd9de578236fc46727a5899
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2024-08-17 06:32:15 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 88 ms, Memory: 44056 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
@@ -1,4 +1,3 @@
-# 틀린 코드 
 import sys 
 import math 
 
@@ -6,26 +5,24 @@ N = int(sys.stdin.readline())
 M = int(sys.stdin.readline())
 light = list(map(int, sys.stdin.readline().split()))
 
-def binary_search():
+def binary_search(maxNum):
     left = 0
     right = N
     answer = 0
 
     while left <= right:
         mid = (left + right) // 2
-
-        maxNum = max(light[0] - 0, N - light[len(light) - 1])
-        for i in range(1, len(light)):
-            diff = math.ceil((light[i] - light[i - 1]) / 2)
-            maxNum = max(diff, maxNum)
         
         if maxNum <= mid:
             right = mid - 1
             answer = mid 
-            # print(answer)
         else:
             left = mid + 1
-            # answer = mid 
     print(answer)
 
-binary_search()
+maxNum = max(light[0] - 0, N - light[len(light) - 1])
+for i in range(1, len(light)):
+    diff = math.ceil((light[i] - light[i - 1]) / 2)
+    maxNum = max(diff, maxNum)
+
+binary_search(maxNum)
