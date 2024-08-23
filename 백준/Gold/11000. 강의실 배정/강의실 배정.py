0bfe18fc6e5945ef252761f6fd4ee684cc9f4d3f
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2024-08-24 01:30:22 +0900

    [Gold V] Title: 강의실 배정, Time: 568 ms, Memory: 61160 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,22 +1,28 @@
-import sys 
-import heapq 
+import sys
+import heapq
 
 n = int(sys.stdin.readline())
+lectureH = []
 lecture = []
+
 for _ in range(n):
     a, b = map(int, sys.stdin.readline().split())
-    heapq.heappush(lecture, (a, 1)) # start 
-    heapq.heappush(lecture, (b, 0)) # end 
+    heapq.heappush(lectureH, (a, b))  
 
-cnt = 0
+end_times = []
 ans = 0
-while lecture:
-    a, b = heapq.heappop(lecture)
-    if b == 1:
-        cnt += 1
-    else:
-        cnt -= 1
 
-    ans = max(cnt, ans)
+while lectureH:
+    start, end = heapq.heappop(lectureH)
+
+    # 현재 진행 중인 강의 중에서 끝난 강의는 제거
+    while end_times and end_times[0] <= start:
+        heapq.heappop(end_times)
+
+    # 현재 강의의 종료 시간을 추가
+    heapq.heappush(end_times, end)
+
+    # 동시에 진행 중인 강의 수의 최대값 갱신
+    ans = max(ans, len(end_times))
+
 print(ans)
-
