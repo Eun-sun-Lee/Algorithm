9cbc3409899f0c7aa6af7b381f71d0997b6887ef
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2024-08-24 03:18:20 +0900

    [Silver III] Title: 달팽이, Time: 568 ms, Memory: 70028 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -6,9 +6,9 @@ dy = [-1, 0, 1, 0] # 위, 오, 아, 좌
 dx = [0, 1, 0, -1] # 위, 오, 아, 좌
 
 graph = [[0] * n for _ in range(n)]
+ans = []
 
 def sol():
-
     cnt = 1
     y = n // 2 
     x = n // 2 
@@ -21,22 +21,19 @@ def sol():
                 i += 1
 
             for _ in range(i):
+                if cnt == k:
+                    ans.append(y + 1)
+                    ans.append(x + 1)
+
                 if cnt >= n * n: return
 
                 cnt += 1
                 y += dy[j]
                 x += dx[j]
 
-                # print(y, x, cnt)
                 graph[y][x] = cnt 
 
 sol()
-ans = [0, 0]
-for i in range(n):
-    print(*graph[i])
-    for j in range(n):
-        if graph[i][j] == k:
-            ans[0] = i + 1
-            ans[1] = j + 1
-print(ans[0], ans[1])
-    
+for i in graph:
+    print(*i)
+print(*ans)
