71bb528c21a09a3899e4839211a68aadb2c9f776
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2024-08-23 17:02:17 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,28 +1,18 @@
 import sys 
 t = int(sys.stdin.readline())
 
-def dynamic(n):
+def sol(n):
     dp = [0 for _ in range(n + 1)] 
 
     dp[0] = 1
-    dp[1] = 1 
-    dp[2] = 2
-    dp[3] = 4
 
-    for i in range(4, n + 1):
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    for i in range(1, n + 1):
+        for j in range(1, 4):
+            if i - j >= 0:
+                dp[i] += dp[i - j]
     print(dp[n])
 
 
 for _ in range(t):
     n = int(sys.stdin.readline())
-    if n == 0:
-        print(1)
-    elif n == 1:
-        print(1)
-    elif n == 2:
-        print(2)
-    elif n == 3:
-        print(4)
-    else:
-        dynamic(n)
+    sol(n) 
