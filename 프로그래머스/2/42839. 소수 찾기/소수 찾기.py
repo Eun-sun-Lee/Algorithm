0025b822777c0b6f4ea4739f92ef9f49fd978d5a
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2024-09-09 03:30:38 +0900

    [level 2] Title: 소수 찾기, Time: 1042.49 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -8,31 +8,39 @@ def erastosthenes(max_num):
         p += 1
     return set(str(p) for p in range(2, max_num + 1) if is_prime[p])
 
+
+def dfs(level, numbers):
+    global visited, str, result, era_list
+    
+    s = ''.join(str)
+    print(s)
+    
+    if s not in result and s in era_list:
+        result.append(s)
+        # str[level] = ''
+    
+    if level == len(numbers):
+        return
+        
+        
+    for i in range(len(numbers)):
+        if visited[i] == False:
+            visited[i] = True 
+            str[level] = numbers[i]
+            dfs(level + 1, numbers)
+            visited[i] = False
+            str[level] = ''
+            
+    
 def solution(numbers):
+    global visited, str, result, era_list
+    
     max_num = int(''.join(sorted(numbers, reverse=True)))  # 주어진 숫자들로 만들 수 있는 가장 큰 숫자
-    era = erastosthenes(max_num)  # 주어진 숫자로 만들 수 있는 최대 값까지만 소수를 구함
-    answer = 0
-    checked = [False] * len(numbers)
-    current_str = []
-    result = set()
+    era_list = erastosthenes(max_num)
+    visited = [False] * len(numbers)
+    str = ['' for _ in range(len(numbers))]
+    result = []
     
-    def dfs(level):
-        if level > 0:
-            joined_str = ''.join(current_str)
-            if joined_str in era:
-                result.add(joined_str)
-        
-        if level == len(numbers):
-            return
-        
-        for i in range(len(numbers)):
-            if not checked[i]:
-                checked[i] = True
-                current_str.append(numbers[i])
-                dfs(level + 1)
-                checked[i] = False
-                current_str.pop()
+    dfs(0, numbers)
     
-    dfs(0)
-    print(result)
     return len(result)
