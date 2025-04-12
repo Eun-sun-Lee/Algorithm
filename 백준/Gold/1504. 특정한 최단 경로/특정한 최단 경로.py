77f06c0ea6a8f9de3497e17207566b34b346dc8f
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2025-04-12 18:00:03 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 484 ms, Memory: 66168 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -1,36 +1,43 @@
-import sys
-import heapq
-node, edge = map(int, sys.stdin.readline().split())
-graph = [[] for _ in range(node + 1)]
+import heapq 
+import sys 
 
-for i in range(edge):
-    u, v, w = map(int, sys.stdin.readline().split())
-    graph[u].append((v, w))
-    graph[v].append((u, w))
-first, second = map(int, sys.stdin.readline().split())
+n, m = map(int ,sys.stdin.readline().split())
+graph = [[] for _ in range(n + 1)]
+answer = sys.maxsize
+
+for _ in range(m):
+    a, b, w = map(int, sys.stdin.readline().split())
+    graph[a].append((b, w)) # (n1, w)
+    graph[b].append((a, w)) # (n2, w)
+n1, n2 = map(int, sys.stdin.readline().split())
+
 
 def dijkstra(graph, start, end):
+    distance = [sys.maxsize] * (n + 1)
     heap = [(0, start)]
-    distance = [sys.maxsize] * (node + 1)
     distance[start] = 0
 
     while heap:
         curr_dist, u = heapq.heappop(heap)
-        if distance[u] < curr_dist: continue
-        for v, weight in graph[u]:
-            if distance[v] > distance[u] + weight:
-                distance[v] = distance[u] + weight
+
+        if distance[u] < curr_dist : continue 
+
+        for v, w in graph[u]:
+            if distance[v] > distance[u] + w:
+                distance[v] = distance[u] + w 
                 heapq.heappush(heap, (distance[v], v))
+
     return distance[end]
 
-answer = sys.maxsize
-l = [1, first, second, node]
-for i in range(2):
+
+dijkstra(graph, 1, n)
+
+l = [1, n1, n2, n]
+for ii in range(2):
     cnt = 0
-    for j in range(3):
-        cnt += dijkstra(graph, l[j], l[j + 1])
+    for jj in range(3):
+        cnt += dijkstra(graph, l[jj], l[jj + 1])
     answer = min(answer, cnt)
-    l = [1, second, first, node]
-   
+    l = [1, n2, n1, n]
 
 print(-1 if answer == sys.maxsize else answer)
