a76e5c44f6ee54f0b4e54ed5efb383c5a97ddadb
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2025-05-11 13:40:53 +0900

    [level 1] Title: 최소직사각형, Time: 5.59 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,28 +1,8 @@
 def solution(sizes):
-    answer = 0
+    # 항상 긴 쪽을 앞에 오게 정렬
+    sizes = [[max(w, h), min(w, h)] for w, h in sizes]
     
-    w_max = max(sizes, key = lambda x: x[0])[0]
-    h_max = max(sizes, key = lambda x: x[1])[1]
+    max_w = max(s[0] for s in sizes)  # 긴 쪽 중 최댓값
+    max_h = max(s[1] for s in sizes)  # 짧은 쪽 중 최댓값
 
-    if max(w_max, h_max) == w_max: 
-        # n_h_max = h_max
-        for i in range(len(sizes)):
-            # if sizes[i][0] < sizes[i][1] and n_h_max > sizes[i][0]:
-            if sizes[i][0] < sizes[i][1]:
-                sizes[i][1] = sizes[i][0]
-        h_max = max(sizes, key = lambda x: x[1])[1]
-    else:
-        for i in range(len(sizes)):
-            if sizes[i][0] > sizes[i][1]:
-                sizes[i][0] = sizes[i][1]
-        w_max = max(sizes, key = lambda x: x[0])[0]
-        
-    answer = w_max * h_max
-                
-            
-    
-    
-    
-        
-        
-    return answer
+    return max_w * max_h
