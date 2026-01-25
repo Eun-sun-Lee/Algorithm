670dc6e7152207f2277f664bf3457c027af93232
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2026-01-25 20:34:55 +0900

    [Gold V] Title: 동전 2, Time: 200 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -9,10 +9,12 @@ for _ in range(n):
 dp = [10001] * (k + 1)
 dp[0] = 0
 def coin2(n, coin, k):
-    for i in range(1, k + 1):
-        for j in range(n):
-            if i < coin[j] : continue
-            dp[i] = min(dp[i], dp[i - coin[j]] + 1)
+    for i in range(n):
+        for j in range(1, k + 1):
+            if j-coin[i] >= 0:
+                dp[j] = min(dp[j], dp[j-coin[i]] + 1)
+            else:
+                continue
 coin2(n, coin, k)
 
 print(dp[k] if dp[k] != 10001 else -1)
