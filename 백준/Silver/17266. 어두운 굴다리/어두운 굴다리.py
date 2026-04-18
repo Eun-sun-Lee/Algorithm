01f9dc18f23daa3a9bae5fd6fcaef1b6e71b7199
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2026-04-18 16:04:56 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 84 ms, Memory: 44788 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
@@ -1,12 +1,14 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import sys 
+import math 
+
+N = int(sys.stdin.readline())
+M = int(sys.stdin.readline())
+light = list(map(int, sys.stdin.readline().split()))
+
+
+maxNum = max(light[0] - 0, N - light[len(light) - 1])
+for i in range(1, len(light)):
+    diff = math.ceil((light[i] - light[i - 1]) / 2)
+    maxNum = max(diff, maxNum)
+
+print(maxNum)
