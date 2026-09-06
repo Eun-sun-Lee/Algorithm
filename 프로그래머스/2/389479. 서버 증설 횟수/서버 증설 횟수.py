a048c15f038e1e4be5a653c97c881c3cb13fcb66
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2026-09-06 17:41:52 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.04 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
@@ -2,23 +2,30 @@ import heapq
 
 def solution(players, m, k):
     answer = 0
-
+    
     minServCnt = 0
     server = []
 
     for i in range(len(players)):
-        heapq.heappush(server, (i, 1, players[i]))   # 수정
+        heapq.heappush(server, (i, 1, players[i])) # 디버깅 2
+        # heapq.heappush(server, (i + k, 1, need))
 
     while server:
         idx, status, cnt = heapq.heappop(server)
-
-        if status == 1:   # 수정
+        
+        
+        if status == 1: # 디버깅 2
             if (cnt // m) > minServCnt:
-                tmp = (cnt // m - minServCnt)
+                tmp = (cnt // m - minServCnt) # 디버깅 1
                 minServCnt += tmp
                 answer += tmp
-                heapq.heappush(server, (idx + k, 0, tmp))   # 수정
+                heapq.heappush(server, (idx + k, 0, tmp)) # 디버깅 2
         else:
             minServCnt -= cnt
-
+        
+        # print(idx, status, cnt, minServCnt, answer)
+    
+    print(answer)
+            
+        
     return answer
