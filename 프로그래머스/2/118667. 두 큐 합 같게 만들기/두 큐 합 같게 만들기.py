f740592f72ad1a80057df7b4070f617f2466a5b1
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2026-09-18 12:26:38 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 73.45 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -2,38 +2,53 @@ from collections import deque
 
 def solution(queue1, queue2):
     answer = -1
-
-    total = sum(queue1) + sum(queue2)
-
-    # 예외처리
-    if total % 2 != 0:
+    
+    # [수정 1] 전체 합이 홀수면 절대 같게 만들 수 없음
+    if (sum(queue1) + sum(queue2)) % 2 != 0:
         return -1
+    
+    value = (sum(queue1) - sum(queue2)) // 2
 
-    target = total // 2
-
-    q1 = deque(queue1)
-    q2 = deque(queue2)
-
-    sum_1 = sum(queue1)
-
-    cnt = 0
-    max_cnt = (len(queue1) + len(queue2)) * 2
-
-    while cnt < max_cnt:
-
-        if sum_1 == target:
-            return cnt
-
-        if sum_1 > target:
-            num = q1.popleft()
-            sum_1 -= num
-            q2.append(num)
-
+    p1 = p2 = -1
+    sum_1 = 0
+    sum_2 = 0
+    
+    while True:
+        
+        if value == 0: 
+            answer = 0
+            break
+        
+        if sum_1 - sum_2 == value:
+            answer = p1 + p2 + 2
+            break
+        
+        # [수정 2]
+        # 기존: if sum_1 <= sum_2:
+        if sum_1 - sum_2 < value:
+            
+            # [수정 3] p1 자체의 범위를 확인
+            if p1 + 1 >= len(queue1) + len(queue2):
+                break
+            
+            p1 += 1
+            
+            if p1 < len(queue1):
+                sum_1 += queue1[p1]
+            else:
+                sum_1 += queue2[p1 - len(queue1)]
+                
         else:
-            num = q2.popleft()
-            sum_1 += num
-            q1.append(num)
-
-        cnt += 1
-
-    return -1
+            
+            # [수정 3] p2 자체의 범위를 확인
+            if p2 + 1 >= len(queue1) + len(queue2):
+                break
+            
+            p2 += 1
+            
+            if p2 < len(queue2):
+                sum_2 += queue2[p2]
+            else:
+                sum_2 += queue1[p2 - len(queue2)]
+    
+    return answer
