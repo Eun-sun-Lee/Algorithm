c8b63fffb558738ebb900db5afb4ba4f1fb08231
esssun <84428520+Eun-sun-Lee@users.noreply.github.com>
2026-09-26 11:46:15 +0900

    [level 3] Title: 봉인된 주문, Time: 279.92 ms, Memory: 49.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.py
@@ -0,0 +1,28 @@
+def solution(n, bans):
+    def to_num(s):
+        num = 0
+        for c in s:
+            num = num * 26 + (ord(c) - ord('a') + 1)
+        return num
+
+    # 봉인된 주문을 숫자로 변환
+    bans = [to_num(ban) for ban in bans]
+    bans.sort()
+
+    # n번째 주문을 찾으면서,
+    # 앞에서 봉인된 주문을 만나면 그만큼 뒤로 밀어준다.
+    for ban in bans:
+        if ban <= n:
+            n += 1
+        else:
+            break
+
+    # 숫자를 주문 문자열로 변환
+    answer = ''
+
+    while n > 0:
+        n -= 1
+        answer = chr(n % 26 + ord('a')) + answer
+        n //= 26
+
+    return answer
